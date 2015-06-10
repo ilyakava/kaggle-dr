17e835f51d0dc6ccb11ebd95653a463322766be6
Ilya Kavalerov <ilya@artsymail.com>
2015-06-10 10:39:44 -0400

    Use lasagne in favor of ciresan layers

--- my_code/VGGNet.py
+++ my_code/VGGNet.py
@@ -15,6 +15,8 @@ import theano.tensor as T
 from theano.tensor.signal import downsample
 from theano.tensor.nnet import conv
 from lasagne.nonlinearities import LeakyRectify
+import lasagne
+from lasagne import layers
 # git submodules
 from ciresan.code.logistic_sgd import LogisticRegression, load_data
 from ciresan.code.mlp import HiddenLayer
@@ -26,12 +28,7 @@ from my_code.util import QWK, print_confusion_matrix
 
 import pdb
 
-class NetworkInput(object):
-    """
-    A dummy class that pretends the input to the network is a 'layer'
-    """
-    def __init__(self, input):
-        self.output = input
+MOMENTUM = 0.9
 
 class VGGNet(Ciresan2012Column):
     def __init__(self, datasets, batch_size, cuda_convnet, leakiness, partial_sum, model_spec, pad=1, params=None):
@@ -64,7 +61,7 @@ class VGGNet(Ciresan2012Column):
         learning_rate = T.fscalar()
 
         # start-snippet-1
-        x = T.matrix('x')   # the data is presented as rasterized images
+        x = T.tensor4('x')   # the data is presented as rasterized images
         y = T.ivector('y')  # the labels are presented as 1D vector of
                             # [int] labels
         lr = LeakyRectify(leakiness)
@@ -73,21 +70,8 @@ class VGGNet(Ciresan2012Column):
         # BUILD ACTUAL MODEL #
         ######################
         print '... building the column'
-        # NOTE: the word "layer" in the comments and variables refers to an object
-        # that can be instantiated, i.e. "layer" is an operation in the CN network
-        # (with inputs and outputs) and therefore refers to a region of the network that
-        # has some inputs and has some outputs. Again, "layer" does not refer to a group
-        # of parameters or a block of weights. Rather, "layer" is an object that can be
-        # instantiated and has params/weights as its attributes, as well as input/output
-        # as attributes
 
-        # list to hold layers, each element must have an `output` property,
-        # each element after the first one must also have a `params` property
-        layers = [None] * len(model_spec) # stick in the images as "0th" layer
-        # TODO get rid of dimshuffle
-        raw_image_data = x.reshape((batch_size, model_spec[0][1], model_spec[0][0], model_spec[0][0]))
-        network_input = raw_image_data.dimshuffle(1, 2, 3, 0) if cuda_convnet else raw_image_data
-        layers[0] = NetworkInput(network_input)
+        all_layers = [layers.InputLayer(shape=(None, model_spec[0][1], model_spec[0][0], model_spec[0][0]))]
 
         # precompute layer sizes
         layer_input_sizes = numpy.ones(len(model_spec), dtype=int)
@@ -107,7 +91,7 @@ class VGGNet(Ciresan2012Column):
                     layer_input_sizes[i] = ((layer_input_sizes[i-1] - model_spec[i-1][0]) / downsample) + 1
 
         # print some info
-        print "[DEBUG] layers input widths: {}".format(layer_input_sizes)
+        print "[DEBUG] all_layers input widths: {}".format(layer_input_sizes)
         layer_parameter_counts = [0] + [model_spec[i - 1][1]*layer_input_sizes[i]**2 for i in xrange(1,len(model_spec))]
         print "[DEBUG] layer parameter sizes: {}".format(layer_parameter_counts)
 
@@ -119,26 +103,26 @@ class VGGNet(Ciresan2012Column):
             else:
                 layer_weight_count[i] *= batch_size
         print "[DEBUG] layer weight counts: {}".format(layer_weight_count)
-        # warning when FC layers are too big
-        FC_layers = numpy.array([-1] + [len(layer_spec) for layer_spec in model_spec[1:]]) == 2 # -1 to skip input
-        FC_layers_descending = layer_weight_count[FC_layers].tolist() == sorted(layer_weight_count[FC_layers])[::-1]
-        if not FC_layers_descending:
+        # warning when FC all_layers are too big
+        FC_all_layers = numpy.array([-1] + [len(layer_spec) for layer_spec in model_spec[1:]]) == 2 # -1 to skip input
+        FC_all_layers_descending = layer_weight_count[FC_all_layers].tolist() == sorted(layer_weight_count[FC_all_layers])[::-1]
+        if not FC_all_layers_descending:
             print SKULL
-            print "[WARNING] FC layers are not descending in weight counts"
-        # info for weight increase between conv and FC layers
-        first_FC_layer_index = min(numpy.where(FC_layers)[0])
+            print "[WARNING] FC all_layers are not descending in weight counts"
+        # info for weight increase between conv and FC all_layers
+        first_FC_layer_index = min(numpy.where(FC_all_layers)[0])
         weight_increase_into_FC = layer_weight_count[first_FC_layer_index] / layer_weight_count[first_FC_layer_index-1]
         print "[INFO] Weights increased %f X from last CONV into first FC layer" % weight_increase_into_FC
         # upto 50ish probably ok (network on: http://cs231n.github.io/convolutional-networks/)
 
-        # create layers
+        # create all_layers
         for i in xrange(1,len(model_spec)):
             cs = model_spec[i] # current spec
             iz = layer_input_sizes[i] # input size
             ps = model_spec[i - 1] # previous spec
 
             prev_layer_params = ps[1] * iz**2
-            fltargs = dict(n_in=prev_layer_params, n_out=cs[1]) # n_out only relevant for FC layers
+            fltargs = dict(n_in=prev_layer_params, n_out=cs[1]) # n_out only relevant for FC all_layers
             print "[DEBUG] Layer %i" % i
             if len(cs) >= 3: # conv layer
                 image_shape = (ps[1], iz, iz, batch_size) if cuda_convnet else (batch_size, ps[1], iz, iz)
@@ -146,58 +130,52 @@ class VGGNet(Ciresan2012Column):
                 print image_shape
                 print filter_shape
                 border_mode = 'full' if pad else 'valid'
-                layers[i] = LeNetConvPoolLayer(
-                    rng,
-                    input=layers[i-1].output,
-                    filter_shape=filter_shape, # (prev_thickness, convx, convy, cur_thickness)
-                    image_shape=image_shape, # (prev_thickness, cur_size, cur_size, bs)
-                    poolsize=(cs[-1], cs[2]), # ds, stride
-                    cuda_convnet=cuda_convnet,
-                    activation=lr,
-                    border_mode=border_mode,
-                    partial_sum=partial_sum,
-                    pad=pad
-                )
+                all_layers.append(layers.Conv2DLayer(all_layers[-1],
+                                    num_filters=image_shape[1],
+                                    filter_size=(filter_shape[2], filter_shape[3]),
+                                    W=lasagne.init.Normal()))
+                all_layers.append(layers.MaxPool2DLayer(all_layers[-1], (cs[-1], cs[-1])))
             elif i == (len(model_spec) - 1): # last softmax layer
                 print fltargs
                 assert(len(ps) == 2) # must follow an FC layer
-                layers[i] = LogisticRegression(input=layers[i-1].output, **fltargs)
+                all_layers.append(layers.DenseLayer(all_layers[-1],
+                                   num_units=1,
+                                   nonlinearity=None))
             elif len(cs) == 2: # FC layer
                 print fltargs
-                raw_in = layers[i-1].output
-                if len(ps) >= 3: # previous layer was a conv layer
-                    flt_input = raw_in.dimshuffle(3, 0, 1, 2).flatten(2) if cuda_convnet else raw_in.flatten(2)
-                else:
-                    flt_input = raw_in
-                layers[i] = HiddenLayer(rng, input=flt_input, activation=T.tanh, **fltargs)
+                all_layers.append((layers.DenseLayer(all_layers[-1],
+                                   num_units=cs[1],
+                                   W=lasagne.init.Normal())))
+                all_layers.append(lasagne.layers.DropoutLayer(all_layers[-1], p=0.5))
 
         # going off of: http://cs231n.github.io/convolutional-networks/
         print "[INFO] Estimated memory usage is %f MB per input image" % round(sum(layer_parameter_counts) * 4e-6 * 3, 2)
-        # TODO change this to the kappa loss
-        cost = layers[-1].negative_log_likelihood(y)
+        objective = lasagne.objectives.Objective(all_layers[-1], loss_function=lasagne.objectives.mse)
+        cost = objective.get_loss(x, target=y.reshape((batch_size,1)))
 
-        # create a function to compute the mistakes that are made by the model
+        # return error of train batch
         self.test_model = theano.function(
             [index],
-            layers[-1].errors(y),
+            cost,
             givens={
                 x: test_set_x[index * batch_size: (index + 1) * batch_size],
                 y: test_set_y[index * batch_size: (index + 1) * batch_size]
             }
         )
 
-        # create a function to compute probabilities of all output classes
+        # return outputs of valid batch
         self.valid_output_batch = theano.function(
             [index],
-            layers[-1].p_y_given_x,
+            T.iround(lasagne.layers.get_output(all_layers[-1], x, deterministic=True)),
             givens={
                 x: valid_set_x[index * batch_size: (index + 1) * batch_size]
             }
         )
 
+        # return error of valid batch
         self.validate_model = theano.function(
             [index],
-            layers[-1].errors(y),
+            objective.get_loss(x, target=y.reshape((batch_size,1)), deterministic=True),
             givens={
                 x: valid_set_x[index * batch_size: (index + 1) * batch_size],
                 y: valid_set_y[index * batch_size: (index + 1) * batch_size]
@@ -205,33 +183,21 @@ class VGGNet(Ciresan2012Column):
         )
 
         # create a list of all model parameters to be fit by gradient descent
-        nonflat_params = [layer.params for layer in layers[1:]]
-        self.params = [item for sublist in nonflat_params for item in sublist]
-
-        # create a list of gradients for all model parameters
-        grads = T.grad(cost, self.params)
-
-        # train_model is a function that updates the model parameters by
-        # SGD Since this model has many parameters, it would be tedious to
-        # manually create an update rule for each model parameter. We thus
-        # create the updates list by automatically looping over all
-        # (params[i], grads[i]) pairs.
-        updates = [
-            (param_i, param_i - (learning_rate) * grad_i)
-            for param_i, grad_i in zip(self.params, grads)
-        ]
+        self.params = lasagne.layers.get_all_params(all_layers[-1])
+        updates = lasagne.updates.nesterov_momentum(cost, self.params, learning_rate, MOMENTUM)
 
         # Suggested by Alex Krizhevsky, found on:
         # http://yyue.blogspot.com/2015/01/a-brief-overview-of-deep-learning.html
-        optimal_ratio = 0.001
-        # should show what multiple current learning rate is of optimal learning rate
-        grads_L1 = sum([abs(grad).sum() for grad in grads])
-        params_L1 = sum([abs(param).sum() for param in self.params])
-        update_ratio = (learning_rate/(optimal_ratio)) * (grads_L1/params_L1)
+        # optimal_ratio = 0.001
+        # # should show what multiple current learning rate is of optimal learning rate
+        # grads = T.grad(cost, self.params)
+        # grads_L1 = sum([abs(grad).sum() for grad in grads])
+        # params_L1 = sum([abs(param).sum() for param in self.params])
+        # update_ratio = (learning_rate/(optimal_ratio)) * (grads_L1/params_L1)
 
         self.train_model = theano.function(
             [index, learning_rate],
-            [cost, update_ratio],
+            [cost],
             updates=updates,
             givens={
                 x: train_set_x[index * batch_size: (index + 1) * batch_size],
@@ -280,11 +246,11 @@ class VGGNet(Ciresan2012Column):
 
                 iter = (epoch - 1) * self.n_train_batches + minibatch_index
 
-                cost_ij, update_ratio = self.train_model(minibatch_index, cur_learning_rate)
+                cost_ij = self.train_model(minibatch_index, cur_learning_rate)
                 self.historical_costs.append([iter, cost_ij])
 
                 if iter % 100 == 0:
-                    print 'training @ iter = %i. Cur learning rate (%f) is %f x optimal' % (iter, cur_learning_rate, update_ratio)
+                    print 'training @ iter = %i. Cur learning rate is %f' % (iter, cur_learning_rate)
 
                 if (iter + 1) % validation_frequency == 0:
 
