75073db9536901722b957b8d6a76249eea2576c9
Ilya Kavalerov <ilya@artsymail.com>
2015-06-23 15:35:24 -0400

    Adds two modes for preparing images

--- data/create_resize_batchfiles.py
+++ data/create_resize_batchfiles.py
@@ -31,6 +31,25 @@ def centered_crop_batchfile(files, size, in_dir, out_dir, index):
     out.close()
     return name
 
+def downsize_no_trim_batchfile(files, size, in_dir, out_dir, index):
+    name = "data/convert/downsize_no_trim_batchfile_%i.txt" % index
+    out = open(name, 'w')
+    for f in files:
+        cmd = 'convert %s%s -scale %ix%i -quality 100 -normalize %s%s\n' % (in_dir, f, size, size, out_dir, f)
+        out.write (cmd)
+    out.close()
+    return name
+
+def centered_normalized_crop_batchfile(files, size, in_dir, out_dir, index):
+    name = "data/convert/centered_normalized_crop_batchfile_%i.txt" % index
+    out = open(name, 'w')
+    for f_ in files:
+        f = '.'.join([f_.split('.')[0], 'png'])
+        cmd = 'convert %s%s -format png -normalize -bordercolor black -border 1x1 -fuzz 10%% -trim +repage -gravity center -resize %i -background black -gravity center -extent %ix%i %s%s\n' % (in_dir, f_, size, size, size, out_dir, f)
+        out.write (cmd)
+    out.close()
+    return name
+
 if __name__ == '__main__':
     arg_names = ['command', 'in_dir', 'out_dir', 'mode', 'size', 'num_batch_files']
     arg = dict(zip(arg_names, sys.argv))
@@ -53,6 +72,10 @@ if __name__ == '__main__':
         files = gm_batchfiles(groups, size, in_dir, out_dir, simple_crop_batchfile)
     elif mode == 2:
         files = gm_batchfiles(groups, size, in_dir, out_dir, centered_crop_batchfile)
+    elif mode == 3:
+        files = gm_batchfiles(groups, size, in_dir, out_dir, downsize_no_trim_batchfile)
+    elif mode == 4:
+        files = gm_batchfiles(groups, size, in_dir, out_dir, centered_normalized_crop_batchfile)
     else:
         raise ValueError("unsupported mode %i" % mode)
 
