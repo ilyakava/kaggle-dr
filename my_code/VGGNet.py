ff88bd8450542515c33bcb6501ae2f168f8c8556
Ilya Kavalerov <ilya@artsymail.com>
2015-08-11 12:13:36 -0400

    move to highest layer (looks like smaller step took care of image vanishing problem)

--- my_code/VGGNet.py
+++ my_code/VGGNet.py
@@ -104,7 +104,7 @@ class VGGNet(object):
             },
         )
 
-        layer_idx_of_interest = 8
+        layer_idx_of_interest = 16
         my_input = X_batch
         l2_activations = T.sum(lasagne.layers.get_output(self.all_layers[layer_idx_of_interest], my_input, deterministic=True) ** 2)
         dream_updates = lasagne.updates.sgd(l2_activations, [my_input], learning_rate)
