a0d105edeb66953deaae6708f11ab582e45a7425
Ilya Kavalerov <ilya@artsymail.com>
2015-08-11 21:55:35 -0400

    avoid reshape for grey

--- my_code/data_stream.py
+++ my_code/data_stream.py
@@ -252,7 +252,11 @@ class DataStream(object):
         as_grey = True if self.image_shape[2] == 1 else False
         img = imread(image_dir + image_name + extension, as_grey=as_grey)
         img = self.crop_image(img, crop_lambda) if crop_lambda else img
-        return (img.reshape(self.image_shape) / 255.) # reshape in case it is as_grey
+        img = img / 255.
+        if len(img.shape) == 2:
+            return img.reshape(img.shape + (1,)) # when grey, img might lack dimension
+        else:
+            return img
 
     def preprocess_image(self, image, flip_coords, color_cast):
         """
