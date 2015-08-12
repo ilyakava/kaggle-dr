eba9f7fa018b51c01435a33a2a59d89ab61a0bc3
Ilya Kavalerov <ilya@artsymail.com>
2015-08-12 09:39:07 -0400

    one more assert on zoom

--- my_code/plot_dreams.py
+++ my_code/plot_dreams.py
@@ -113,6 +113,7 @@ class DreamStudyBuffer(object):
         for normalized_octave_image in normalized_octave_images[1:]:
             new_zoom = (self.source_size / numpy.array(normalized_octave_image.shape[:2])).tolist() + [1]
             enlarged = nd.zoom(normalized_octave_image, new_zoom, order=1)
+            assert(list(enlarged.shape[:2]) == self.source_size.tolist())
             cumulative_image += enlarged
 
         self.source = cumulative_image
