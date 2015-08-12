22cd59b46544ac3c681793d58c623d089d24b420
Ilya Kavalerov <ilya@artsymail.com>
2015-08-11 23:40:08 -0400

    pdb

--- my_code/plot_dreams.py
+++ my_code/plot_dreams.py
@@ -103,7 +103,7 @@ class DreamStudyBuffer(object):
                 octave_acc[t:b,l:r,:] += 1
                 idx += 1
         normalized_octave_images = [octave_images[i] / (len(self.octave_sizes)*octave_accs[i]) for i in range(len(octave_images))]
-
+        pdb.set_trace()
         # enlarge each octave to original image size and update source image
         cumulative_gradient = normalized_octave_images[0]
         for normalized_octave_image in normalized_octave_images[1:]:
